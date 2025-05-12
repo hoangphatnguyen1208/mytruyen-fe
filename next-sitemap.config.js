9b6245e18610e741c6524e7b5ebca203cb4427cd
Nguyen Hoang Phat <88542260+hoangphat120805@users.noreply.github.com>
2025-05-12 10:40:18 +0700

    docs: add sitemap to robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -13,34 +13,34 @@ module.exports = {
             lastmod: new Date().toISOString()
         }
     },
-    additionalPaths: async (config) => {
-        let page = 1
-        const paths = []
-        while (true) {
-            console.log("Fetching page", page)
-            const res = await fetch(
-                `https://backend.metruyencv.com/api/books?page=${page}&limit=200`
-            )
-            if (!res.ok) {
-                console.error("Failed to fetch data", res.statusText)
-                break
-            }
-            const data = await res.json()
-            if (data.pagination.next === null) {
-                break
-            }
-            paths.push(
-                ...data.data.map((item) => {
-                    return {
-                        loc: `/truyen/${item.slug}`,
-                        changefreq: "daily",
-                        priority: 0.7,
-                        lastmod: new Date().toISOString()
-                    }
-                })
-            )
-            page++
-        }
-        return paths
-    }
+    // additionalPaths: async (config) => {
+    //     let page = 1
+    //     const paths = []
+    //     while (true) {
+    //         console.log("Fetching page", page)
+    //         const res = await fetch(
+    //             `https://backend.metruyencv.com/api/books?page=${page}&limit=200`
+    //         )
+    //         if (!res.ok) {
+    //             console.error("Failed to fetch data", res.statusText)
+    //             break
+    //         }
+    //         const data = await res.json()
+    //         if (data.pagination.next === null) {
+    //             break
+    //         }
+    //         paths.push(
+    //             ...data.data.map((item) => {
+    //                 return {
+    //                     loc: `/truyen/${item.slug}`,
+    //                     changefreq: "daily",
+    //                     priority: 0.7,
+    //                     lastmod: new Date().toISOString()
+    //                 }
+    //             })
+    //         )
+    //         page++
+    //     }
+    //     return paths
+    // }
 }
